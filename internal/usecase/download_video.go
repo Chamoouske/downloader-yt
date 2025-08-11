83e37fc419fae6b655aea2a917d17a39d739f0a8
Ajax Lima <ajaxlima94@gmail.com>
2025-08-10 21:35:29 -0300

    refactor: alterando algumas configuracoes

--- internal/usecase/download_video.go
+++ internal/usecase/download_video.go
@@ -1,13 +1,13 @@
 package usecase
 
 import (
-  "downloader/internal/domain"
+	"downloader/internal/domain"
 )
 
 type DownloadVideoUseCase struct {
-    Downloader domain.Downloader
+	Downloader domain.Downloader
 }
 
-func (uc *DownloadVideoUseCase) Execute(video domain.Video, progress domain.ProgressBar) error {
-    return uc.Downloader.Download(video, progress)
+func (uc *DownloadVideoUseCase) Execute(url string, progress domain.ProgressBar) error {
+	return uc.Downloader.Download(domain.Video{URL: url}, progress)
 }
