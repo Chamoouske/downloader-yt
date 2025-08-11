61e7879634304042efcc9dc0cfbba8195aadf3df
Ajax Lima <ajaxlima94@gmail.com>
2025-08-10 23:07:02 -0300

    feat: implementando webserver para download do video

---
+++ cmd/web/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	webserver "downloader/internal/infra/web_server"
+	logger "downloader/pkg/log"
+	"flag"
+	"os"
+)
+
+var log = logger.GetLogger("server")
+var port = flag.Int("p", 0, "Port must not be null")
+
+func main() {
+	flag.Parse()
+
+	if *port == 0 {
+		flag.Usage()
+		log.Error("Usage: downloader -p <port>")
+		os.Exit(1)
+	}
+
+	svr := webserver.NewWebServer()
+
+	svr.Start(*port)
+}
---
+++ internal/infra/web_server/web_server.go
@@ -0,0 +1,68 @@
+package webserver
+
+import (
+	"context"
+	"downloader/internal/infra/progress"
+	"downloader/internal/infra/youtube"
+	"downloader/internal/usecase"
+	logger "downloader/pkg/log"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var log = logger.GetLogger("web_server")
+
+type WebServer struct {
+	server     *http.Server
+	downloadUC usecase.DownloadVideoUseCase
+}
+
+func NewWebServer() *WebServer {
+	downloader := youtube.NewKkdaiDownloader(nil)
+	return &WebServer{downloadUC: usecase.DownloadVideoUseCase{Downloader: downloader}}
+}
+
+func (w *WebServer) Start(port int) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/download", w.listItems)
+
+	w.server = &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: mux,
+	}
+
+	go func() {
+		log.Info(fmt.Sprintf("server listen on port %d", port))
+		if err := w.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Error(fmt.Sprintf("listen: %v", err))
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	w.Stop()
+}
+
+func (w *WebServer) Stop() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_ = w.server.Shutdown(ctx)
+	log.Info("server stopped")
+}
+
+func (ws *WebServer) listItems(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	url := r.URL.Query().Get("url")
+
+	go ws.downloadUC.Execute(url, progress.NewTerminalProgressBar())
+	json.NewEncoder(w).Encode(returnHttp{Message: "Download iniciado"})
+}
+
+type returnHttp struct {
+	Message string `json:"message"`
+}
